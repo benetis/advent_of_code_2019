6e2d4f7752c81442bfd495c023924d55ba4c7052
zygis <zygimantas@benetis.me>
2019-12-01 16:38:10 +0200

    feat(project) initial

---
+++ src/test/scala/me/benetis/HelloSpec.scala
@@ -0,0 +1,9 @@
+package me.benetis
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+//  "The Hello object" should "say hello" in {
+//    Main.greeting shouldEqual "hello"
+//  }
+}
