dbbc8622e98d75e453d7c8c2967b8172f86f684b
zygis <zygimantas@benetis.me>
2019-12-02 10:06:25 +0200

    feat(day2) solution

--- src/main/scala/me/benetis/Main.scala
+++ src/main/scala/me/benetis/Main.scala
@@ -2,8 +2,10 @@ package me.benetis
 
 object Main extends App {
   val programs = Vector(
-    Day1.fuelCounterUpperSum(),
-    Day1.fuelCounterAccountedForFuelSum(),
+//    Day1.fuelCounterUpperSum(),
+//    Day1.fuelCounterAccountedForFuelSum(),
+    Day2.compute(),
+    Day2.stupidInputSearch()
   )
 
   programs.foreach(println)
